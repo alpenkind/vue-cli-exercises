7ffa1270248420f60044141f5024f29660079382
Vanessa Pfohmann <v.pfohmann@googlemail.com>
2024-04-07 13:54:28 +0200

    feat:adding BookListRow Component

---
+++ first-vue-project/src/components/BookListRow.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr class="table-item_table-row">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+    <td><BaseButton text="Add Bookmark" variant="primary" /></td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+export default {
+  components: {
+    BaseButton,
+  },
+  props: {
+    title: String,
+    isbn: String,
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
